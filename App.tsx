af8e09daaee69a28390b7b769cc3f6abc5766a22
aditya-chandraker <aditya.chandraker@uconn.edu>
2023-08-15 11:07:04 -0400

    ui library

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import {
+  Button,
+  lightColors,
+  createTheme,
+  ThemeProvider,
+  Input,
+} from "@rneui/themed";
+
+const theme = createTheme({
+  lightColors: {
+    ...Platform.select({
+      default: lightColors.platform.android,
+      ios: lightColors.platform.ios,
+    }),
+  },
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testing</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Testing</Text>
+        <Button title="My Button" />
+        <Input placeholder="Name" />
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
---
+++ pages/Setup.tsx
@@ -0,0 +1,30 @@
+// export default function Setup() {
+//   return (
+//     <Box alignItems="flex-end" p="8">
+//       <VStack alignItems="flex-end" space="5">
+//         <FormControl>
+//           <FormControl.Label mb="3">
+//             What's your event called?
+//           </FormControl.Label>
+//           <Input placeholder="Event's Name" />
+//         </FormControl>
+//         <FormControl>
+//           <FormControl.Label mb="3">When is your Event?</FormControl.Label>
+//           <Radio.Group nativeID="patani" name="day_night">
+//             <VStack space="3">
+//               <Radio value="day">Day</Radio>
+//               <Radio value="night">Night</Radio>
+//             </VStack>
+//           </Radio.Group>
+//         </FormControl>
+//         <Divider />
+//         <Checkbox size="sm" value="tnc" justifyContent="center" mb="4">
+//           I agree to Terms and conditions
+//         </Checkbox>
+//       </VStack>
+//       <Button mt="2" endIcon={<AddIcon size="3" />}>
+//         Create Event
+//       </Button>
+//     </Box>
+//   );
+// }
