0aa8261dc118854b4472a74bf0d5d74d8e2d3d30
fourducks <ben09duck@gmail.com>
2023-10-17 17:05:12 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
   Button,
   lightColors,
@@ -18,16 +20,49 @@ const theme = createTheme({
   },
 });
 
-export default function App() {
+function LoginScreen({ navigation }) {
   return (
     <ThemeProvider theme={theme}>
       <View style={styles.container}>
-        <Text>Testing</Text>
-        <Button title="My Button" />
+        <Text>Login with your NetID or Google Account</Text>
+        <Button
+          title="Login with NetID"
+          onPress={() => alert('NetID Pressed')} />
+        <Button
+          title="Login with Google"
+          onPress={() => {alert('Google Pressed'); navigation.navigate("Signup")}} />
         <Input placeholder="Name" />
         <StatusBar style="auto" />
       </View>
     </ThemeProvider>
+  );
+}
+
+function SignupScreen({ navigation }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Input placeholder="First Name" />
+        <Input placeholder="Last Name"/>
+        <Button
+          title="Back to Login"
+          onPress={() => navigation.goBack()}/>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -41,3 +76,5 @@ const styles = StyleSheet.create({
     borderColor: "red",
   },
 });
+
+export default App;
